6744ce568983a09d25a03b969adfd63887456eda
kamranjune <khsd6920@gmail.com>
2019-01-25 12:31:05 -0800

    Updates

---
+++ precache-manifest.2f94aaa55c0412683cd4dc7b5ac19fcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "09002831a8b9b2580b4b",
+    "url": "/clickygame/static/js/main.09002831.chunk.js"
+  },
+  {
+    "revision": "168aac664a240aff39b3",
+    "url": "/clickygame/static/js/1.168aac66.chunk.js"
+  },
+  {
+    "revision": "09002831a8b9b2580b4b",
+    "url": "/clickygame/static/css/main.82431c8a.chunk.css"
+  },
+  {
+    "revision": "861c440b7cdfb3f75fce14514e652b3f",
+    "url": "/clickygame/index.html"
+  }
+];
