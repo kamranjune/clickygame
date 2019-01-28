de1e11e374136f3c2bec21b6ba97345d1de9f901
kamranjune <khsd6920@gmail.com>
2019-01-28 11:35:17 -0800

    httpS on the punchbuggy image

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Score(props) {
   return (<div className="score">
   <h1>SCORE: {props.score} || HIGH SCORE: {props.highScore}</h1>
-  <img alt="punchbuggy" src="http://kamranjune.github.io/clickygame/assets/images/punchbuggy.jpg"/>
+  <img alt="punchbuggy" src="https://kamranjune.github.io/clickygame/assets/images/punchbuggy.jpg"/>
   <br/>
   
 
